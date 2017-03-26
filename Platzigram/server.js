a020a56e054b6bd17c94ae940f433dfaa28f0902
Julio Rodriguez <juliorr@gmail.com>
2017-03-26 11:47:59 -0700

    First commit

---
+++ Platzigram/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/signup', function (req, res) {
+  res.render('index');
+});
+
+app.get('/signin', function (req, res) {
+  res.render('index');
+});
+
+app.listen(80, function (err) {
+  if (err) {
+    return console.log('Hubo un error'), process.exit(1);
+  } else {
+    console.log('Platzigram echuchando en el puerto 80');
+  }
+});
