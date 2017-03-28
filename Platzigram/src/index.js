dc8a683d44e6d1081f83cf354282e8fbe8ae379f
Julio Rodriguez <juliorr@gmail.com>
2017-03-27 20:41:19 -0700

    add  i18n with intl-relative

--- Platzigram/src/index.js
+++ Platzigram/src/index.js
@@ -1,5 +1,4 @@
 var page = require('page');
-require('moment/locale/es');
 
 require('./homepage');
 require('./signup');
--- Platzigram/src/picture-card/index.js
+++ Platzigram/src/picture-card/index.js
@@ -1,5 +1,10 @@
 var yo = require('yo-yo');
-var moment = require('moment');
+var IntlRelativeFormat = window.IntlRelativeFormat = require('intl-relativeformat');
+
+require('intl-relativeformat/dist/locale-data/en.js');
+require('intl-relativeformat/dist/locale-data/es.js');
+
+var rf = new IntlRelativeFormat('es');
 
 module.exports = function pictureCard(pic) {
   var el;
@@ -14,7 +19,7 @@ module.exports = function pictureCard(pic) {
             <img class="avatar" src="${picture.user.avatar}" />
             <span class="username"> ${picture.user.username}</span>
           </a>
-          <small class="right time">${moment(picture.createAt).fromNow()}</small>
+          <small class="right time">${rf.format(picture.createAt)}</small>
           <p>
             <a class="left" href="#" onclick=${like.bind(null, true)}>
               <i class="fa fa-heart-o" aria-hidden="true"></i>
