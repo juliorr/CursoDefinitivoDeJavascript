4d4a2002248fbdcd1057b9d809b1b4f60c34dd39
Julio Rodriguez <juliorr@gmail.com>
2017-03-30 22:09:47 -0700

    change the language

--- Platzigram/src/signin/template.js
+++ Platzigram/src/signin/template.js
@@ -1,28 +1,28 @@
 var yo = require('yo-yo');
 var landing = require('../landing');
+var translate = require('../translate');
 
-var signinForm = yo `<div class="col s12 m7 ">
+var signinForm = yo`<div class="col s12 m7 ">
                             <div class="row">
                                 <div class="signup-box">
                                     <h1 class="platzigram">Platzigram</h1>
                                     <form class="signup-form">
                                         <div class="section">
-                                            <a class="btn btn-fb hide-on-small-only">Iniciar session con Facebook</a>
-                                            <a class="btn btn-fb hide-on-med-and-up"><i class="fa fa-facebook-official"></i>Iniciar session</a>
+                                            <a class="btn btn-fb hide-on-small-only">${translate.message('signup.facebook')}</a>
+                                            <a class="btn btn-fb hide-on-med-and-up"><i class="fa fa-facebook-official"></i>${translate.message('signup.text')}</a>
                                         </div>
                                         <div class="divider"></div>
                                         <div class="section">
-                  
-                                            <input type="text" name="username" placeholder="Nombre de usuario"/>
-                                            <input type="password" name="password" placeholder="Contraseña"/>
-                                            <button class="btn waves-efect waves-liht btn-signup" type="submit">Inicia Secion</button>
+                                            <input type="text" name="username" placeholder="${translate.message('username')}"/>
+                                            <input type="password" name="password" placeholder="${translate.message('password')}"/>
+                                            <button class="btn waves-efect waves-liht btn-signup" type="submit">${translate.message('signup.text')}</button>
                                         </div>
                                     </form>
                                 </div>
                                 </br>
                                 <div class="row">
                                     <div class="login-box">
-                                        No Tienes una cuenta ? <a href="/signup">Registrate</a>
+                                        ${translate.message('signin.not-have-account')} <a href="/signup">${translate.message('signup.call-to-action')}</a>
                                     </div>
                                 </div>
                             </div>`;
