87b5b5645168e316296335b0d5f7f530a5112a19
Julio Rodriguez <juliorr@gmail.com>
2017-04-04 19:59:43 -0700

    change get data from api

--- Platzigram/server.js
+++ Platzigram/server.js
@@ -6,15 +6,51 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.render('index', {title: 'Platzigram'});
+  res.render('index', {
+    title: 'Platzigram'
+  });
 });
 
 app.get('/signup', function (req, res) {
-  res.render('index', {title: 'Platzigram - Signup'});
+  res.render('index', {
+    title: 'Platzigram - Signup'
+  });
 });
 
 app.get('/signin', function (req, res) {
-  res.render('index', {title: 'Platzigram - Signin'});
+  res.render('index', {
+    title: 'Platzigram - Signin'
+  });
+});
+
+app.get('/api/pictures', function (req, res) {
+  var pictures = [
+      {
+        user: {
+          username: 'Julio',
+          avatar: "https://scontent-dft4-2.xx.fbcdn.net/v/t1.0-9/12400842_10153286774571319_2541176558973363503_n.jpg?oh=8fcd3f41d33243f569bbaf5ed597c585&oe=5956E219" 
+        },
+        url: 'office.jpeg',
+        likes: 0,
+        liked: false,
+        createAt: new Date().getTime()
+      },
+      {
+        user: {
+          username: 'Julio',
+          avatar: "https://scontent-dft4-2.xx.fbcdn.net/v/t1.0-9/12400842_10153286774571319_2541176558973363503_n.jpg?oh=8fcd3f41d33243f569bbaf5ed597c585&oe=5956E219" 
+        },
+        url: 'office.jpeg',
+        likes: 2,
+        liked: true,
+        createAt: new Date().setDate(new Date().getDate() - 10)
+      },
+    ];
+    setTimeout(
+      function() {
+        res.send(pictures);
+      },
+      2000);
 });
 
 app.listen(80, function (err) {
--- Platzigram/src/homepage/index.js
+++ Platzigram/src/homepage/index.js
@@ -2,31 +2,25 @@ var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
 var title = require('title');
+var request = require('superagent');
+var header = require('../header');
 
-page('/', function (ctx, next) {
+page('/', header, loadPictures, function (ctx, next) {
   title('Platzigram');
   var main = document.getElementById('main-container');
-  var pictures = [
-    {
-      user: {
-        username: 'Julio',
-        avatar: "https://scontent-dft4-2.xx.fbcdn.net/v/t1.0-9/12400842_10153286774571319_2541176558973363503_n.jpg?oh=8fcd3f41d33243f569bbaf5ed597c585&oe=5956E219" 
-      },
-      url: 'office.jpeg',
-      likes: 0,
-      liked: false,
-      createAt: new Date()
-    },
-    {
-      user: {
-        username: 'Julio',
-        avatar: "https://scontent-dft4-2.xx.fbcdn.net/v/t1.0-9/12400842_10153286774571319_2541176558973363503_n.jpg?oh=8fcd3f41d33243f569bbaf5ed597c585&oe=5956E219" 
-      },
-      url: 'office.jpeg',
-      likes: 2,
-      liked: true,
-      createAt: new Date().setDate(new Date().getDate() - 10)
-    },
-  ];
-  empty(main).appendChild(template(pictures));
+  
+  empty(main).appendChild(template(ctx.pictures));
 });
+
+function loadPictures(ctx, next) {
+  request
+    .get('/api/pictures')
+    .end(function (err, res){
+      if (err) {
+        return console.log(err);
+      } else {
+        ctx.pictures = res.body;
+        next();
+      }
+    });
+}
